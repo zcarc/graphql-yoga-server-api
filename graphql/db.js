889dca9425cc45660472e05c98a32ae6b86428af
wrkbr <zcarc1@gmail.com>
2020-03-27 13:15:50 +0900

    #12 Wrapping a REST API with GraphQL Part Two

--- graphql/db.js
+++ graphql/db.js
@@ -1,9 +1,20 @@
 import fetch from 'node-fetch';
 
-const API_URL = "https://yst.am/api/v2/list_movies.json?limit=50&minimum_rating=9";
+const API_URL = "https://yst.am/api/v2/list_movies.json?";
 
 export const getMovies = (limit, rating) => {
-  fetch(`${API_URL}`)
+
+  let REQUEST_URL = API_URL;
+
+  if(limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
+  }
+
+  if(rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+
+  return fetch(REQUEST_URL)
     .then(res => res.json())
     .then(json => json.data.movies);
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,12 +2,13 @@ import {getMovies} from './db';
 
 
 // resolvers는 어떤 형태로든 원하는 대로 변화할 수 있다. (example: go to any api or any db)
+// 첫번째 파라미터의 '_'는 parent도 될 수 있고 root도 될 수 있는데 안쓰니까 저렇게 표현한다.
 
 const resolvers = {
   // person: (_, { id }) === person: (_, args.id)
 
   Query: {
-    movies: () => getMovies(),
+    movies: (_, {limit, rating}) => getMovies(limit, rating),
   },
 
 };
